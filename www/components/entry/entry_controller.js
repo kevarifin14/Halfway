dcfc1be56ee7dacd133db7651e4e711b0b72676f
Kevin Arifin <kevarifin14@gmail.com>
2016-01-12 00:07:13 -0800

    Ionic modals for login and signup - only thing that doesnt work right now is animations

--- www/components/entry/entry_controller.js
+++ www/components/entry/entry_controller.js
@@ -2,12 +2,96 @@ angular.module('entry_controller', [])
 
 .controller ('EntryCtrl', function(
   $scope,
-  $location
+  $location,
+  $ionicModal,
+  UserSession,
+  NewUserSession,
+  $rootScope,
+  $ionicPopup,
+  $ionicLoading,
+  $ionicHistory
 ) {
   $scope.showLogin = function() {
     $location.path('/login');
   }
   $scope.showSignup = function() {
     $location.path('/signup');
+  }
+
+ $ionicModal.fromTemplateUrl('login.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.login = modal;
+  });
+
+ $ionicModal.fromTemplateUrl('signup.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.signup = modal;
+  });
+
+  $scope.data = {};
+
+  $scope.loginUser = function() {
+    $ionicLoading.show();
+    $scope.login.hide();
+    var user_session = new UserSession({
+      username: $scope.data.username,
+      password: $scope.data.password,
+    });
+    user_session.$save(
+      function(data) {
+        window.localStorage['userId'] = data.user_id;
+        window.localStorage['username'] = data.username;
+        window.localStorage['userEmail'] = data.email;
+        window.localStorage['longitude'] = data.longitude;
+        window.localStorage['latitude'] = data.latitude;
+        window.localStorage['userAccessToken'] = data.access_token;
+        window.localStorage['profilePicture'] = data.avatar;
+
+        $location.path('/app/halfway');
+        $ionicLoading.hide();
+      },
+      function(err) {
+        var error = err['data']['error'] || err.data.join('. ')
+        var confirmPopup = $ionicPopup.alert({
+          title: 'An error occurred',
+          template: error
+        });
+        $ionicLoading.hide();
+      }
+    );
+  }
+
+  $scope.signupUser = function() {
+    $ionicLoading.show();
+    var new_user_session = new NewUserSession({
+      email: $scope.data.email,
+      username: $scope.data.username,
+      password: $scope.data.password,
+      password_confirmation: $scope.data.password_confirmation,
+      latitude: 0,
+      longitude: 0
+    });
+    new_user_session.$save(
+      function(data) {
+        window.localStorage['userId'] = data.user_id
+        window.localStorage['userName'] = data.username
+        window.localStorage['userEmail'] = data.email
+        window.localStorage['userAccessToken'] = data.access_token
+        $location.path('/app/halfway');
+        $ionicLoading.hide();
+      },
+      function(err) {
+        var error = err['data']['error'] || err.data.join('. ')
+        var confirmPopup = $ionicPopup.alert({
+          title: 'An error occurred',
+          template: error
+        });
+        $ionicLoading.hide();
+      }
+    )
   }
 })
