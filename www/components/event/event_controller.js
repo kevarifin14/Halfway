2e3f27a8cbb6dfd013d94e08c88d4bee47253d7e
Kevin Arifin <kevarifin14@gmail.com>
2016-01-13 08:37:24 -0800

    Add map tab to event view page

--- www/components/event/event_controller.js
+++ www/components/event/event_controller.js
@@ -72,4 +72,25 @@ angular.module('event_controller', ['current_user_service'])
         $scope.$broadcast('scroll.refreshComplete');
       })
   }
+
+  $scope.initialize = function() {
+    var latitude = $scope.event.latitude;
+    var longitude = $scope.event.longitude;
+    var myLatlng = new google.maps.LatLng(latitude, longitude);
+
+    var mapOptions = {
+      center: myLatlng,
+      zoom: 18,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"),mapOptions);
+
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+    });
+
+    $scope.map = map;
+  }
 });
