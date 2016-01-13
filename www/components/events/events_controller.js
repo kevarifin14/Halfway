b9ed924b62d48c36ab6d87065718fceb0e62fa03
Mitas Ray <mitas.ray@berkeley.edu>
2016-01-13 14:58:19 -0500

    refresh added to events page

--- www/components/events/events_controller.js
+++ www/components/events/events_controller.js
@@ -10,10 +10,18 @@ angular.module('events_controller', ['current_user_service'])
     CurrentUser
   ) {
   $scope.events = Events.query({ user_id: CurrentUser.id() });
+
+  $scope.doRefresh = function() {
+    debugger;
+    $scope.events = Events.query({ user_id: CurrentUser.id() });
+    $scope.$broadcast('scroll.refreshComplete');
+  }
+
   $scope.showDetails = function(event) {
     $location.path('app/event/' + event.id);
   };
-  $scope.dividerFunction = function(key){
+
+  $scope.dividerFunction = function(key) {
     return key;
   }
 });
