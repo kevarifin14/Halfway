2fbf66f5fbd90790d16ec8503728a851861a1150
Kevin Arifin <kevarifin14@gmail.com>
2016-01-15 21:58:05 -0800

    Clean up unnecessary methods and objects in accounts

--- www/components/account/account_controller.js
+++ www/components/account/account_controller.js
@@ -1,31 +1,16 @@
-angular.module(
-  'account_controller',
-  [
-    'user_service',
-    'events_service',
-    'friends_service'
-  ]
-).controller(
+angular.module('account_controller', [])
+.controller(
   'AccountCtrl',
   function(
-    $scope,
-    $location,
-    $ionicPopup,
-    User,
-    CurrentUser,
-    Events,
-    Friends,
     Camera,
-    $cordovaGeolocation,
-    $ionicNavBarDelegate,
-    $ionicSideMenuDelegate,
-    $cordovaFileTransfer,
+    CurrentUser,
     $ionicLoading,
-    $rootScope
+    $ionicPopup,
+    $location,
+    $cordovaFileTransfer,
+    $rootScope,
+    $scope
   ) {
-    $scope.avatar = window.localStorage['profilePicture'];
-    $scope.current_user = CurrentUser.user();
-
     var headers = { 'Authorization': window.localStorage['userAccessToken'] };
     var options = {
       fileKey: 'avatar',
@@ -36,26 +21,8 @@ angular.module(
       headers: headers
     }
 
-    $scope.initialize = function() {
-      var latitude = localStorage['latitude'];
-      var longitude = localStorage['longitude'];
-      var myLatlng = new google.maps.LatLng(latitude, longitude);
-
-      var mapOptions = {
-        center: myLatlng,
-        zoom: 16,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-
-      var map = new google.maps.Map(document.getElementById("map"),mapOptions);
-
-      var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-      });
-
-      $scope.map = map;
-    }
+    $scope.avatar = window.localStorage['profilePicture'];
+    $scope.current_user = CurrentUser.user();
 
     $scope.upload = function() {
       document.addEventListener('deviceready', function() {
